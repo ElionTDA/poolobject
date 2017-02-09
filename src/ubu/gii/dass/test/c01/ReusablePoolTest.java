4ec3e7d2bcfa474dd74f0e1f8386fd424523342b
ElionTDA <diego.martin.perez5@gmail.com>
2017-02-09 19:23:53 +0100

    Descripción: Comprobamos el funcionamiento del método adquireReusable. Comprobamos que devuelve instancias de reusable, mientras tiene instancias, y en caso de que no tenga que salta una excepción.
    Datos entrada:
    Una instancia del objeto ReusablePool con la que llamaremos al método.
    
    Salida Esperada:
    Mientras existan instancias las devuelve, y cuando no, salta una
    excepción.

--- src/ubu/gii/dass/test/c01/ReusablePoolTest.java
+++ src/ubu/gii/dass/test/c01/ReusablePoolTest.java
@@ -4,12 +4,15 @@
 package ubu.gii.dass.test.c01;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import ubu.gii.dass.c01.NotFreeInstanceException;
+import ubu.gii.dass.c01.Reusable;
 import ubu.gii.dass.c01.ReusablePool;
 
 /**
@@ -40,7 +43,7 @@ public class ReusablePoolTest {
 		ReusablePool rp1 = ReusablePool.getInstance();
 		ReusablePool rp2 = ReusablePool.getInstance();
 
-		assertEquals(rp1, rp2);
+		assertEquals("Test Singleton", rp1, rp2);
 	}
 
 	/**
@@ -48,7 +51,16 @@ public class ReusablePoolTest {
 	 */
 	@Test
 	public void testAcquireReusable() {
-		fail("Not yet implemented");
+		ReusablePool rp = ReusablePool.getInstance();
+		boolean flag = true;
+		while (flag) {
+			try {
+				assertTrue("Comprobamos que devuelva una instancia de Reusable.",
+						rp.acquireReusable() instanceof Reusable);
+			} catch (NotFreeInstanceException e) {
+				flag = false;
+			}
+		}
 	}
 
 	/**
